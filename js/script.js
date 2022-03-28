746b3e4baa7d01e749acdccea99ebb7eb52b2e43
slumber-parties <ghjrdchd@gmail.com>
2022-03-29 02:14:26 +0300

    refactor: formate code

--- js/script.js
+++ js/script.js
@@ -1,323 +1,283 @@
 'use strict'
 
-const main = document.getElementsByTagName('main')[0];
-const selection = document.getElementsByClassName('selection')[0];
-const title = document.getElementsByClassName('main__title')[0];
+const main = document.getElementsByTagName('main')[0]
+const selection = document.getElementsByClassName('selection')[0]
+const title = document.getElementsByClassName('main__title')[0]
 
 const getData = () => {
-    //промис
-    return fetch('db/quiz_db.json').then(response => response.json());
-};
+  //промис
+  return fetch('db/quiz_db.json').then((response) => response.json())
+}
 
-const showElem = elem => {
+const showElem = (elem) => {
+  let opacity = 0
+  elem.opacity = opacity
+  elem.style.display = ''
 
-    let opacity = 0;
-    elem.opacity = opacity;
-    elem.style.display = '';
+  const animation = () => {
+    opacity += 0.05
+    elem.style.opacity = opacity
 
-    const animation = () => {
-        opacity += 0.05;
-        elem.style.opacity = opacity;
+    if (opacity < 1) {
+      requestAnimationFrame(animation)
+    }
+  }
 
-        if (opacity < 1) {
-            requestAnimationFrame(animation);
-        }
-    };
-
-    requestAnimationFrame(animation);
+  requestAnimationFrame(animation)
 }
 
 const hideElem = (elem, callback) => {
+  let opacity = getComputedStyle(elem).getPropertyValue('opacity')
 
-    let opacity = getComputedStyle(elem).getPropertyValue('opacity');
+  const animation = () => {
+    opacity -= 0.05
+    elem.style.opacity = opacity
 
-    const animation = () => {
-        opacity -= 0.05;
-        elem.style.opacity = opacity;
+    if (opacity > 0) {
+      requestAnimationFrame(animation)
+    } else {
+      elem.style.display = 'none'
+      if (callback) callback()
+    }
+  }
 
-        if (opacity > 0) {
-            requestAnimationFrame(animation)
-        }
-        else {
-            elem.style.display = 'none';
-            if (callback) callback();
-        }
-    };
+  requestAnimationFrame(animation)
+}
 
-    requestAnimationFrame(animation);
+const renderTheme = (data) => {
+  const list = document.getElementsByClassName('selection__list')[0]
+  list.textContent = ''
 
-};
+  const buttons = []
 
-const renderTheme = data => {
+  for (let i = 0; i < data.length; i++) {
+    const li = document.createElement('li')
+    li.className = 'selection__item'
 
-    const list = document.getElementsByClassName('selection__list')[0];
-    list.textContent = '';
+    const button = document.createElement('button')
 
-    const buttons = [];
+    button.className = 'selection__theme'
+    button.dataset.id = data[i].id
+    button.textContent = data[i].theme
+    li.append(button)
+    const result = loadResult(data[i].id)
 
-    for (let i = 0; i < data.length; i++) {
-        const li = document.createElement('li');
-        li.className = 'selection__item';
+    if (result) {
+      const p = document.createElement('p')
 
-        const button = document.createElement('button');
-
-        button.className = 'selection__theme';
-        button.dataset.id = data[i].id;
-        button.textContent = data[i].theme;
-        li.append(button);
-        const result = loadResult(data[i].id);
-
-        if (result) {
-
-            const p = document.createElement('p');
-
-            p.className = 'selection__result';
-            p.innerHTML = `
+      p.className = 'selection__result'
+      p.innerHTML = `
             <span class="selection__result-ratio">${result}/${data[i].list.length}</span>
-            <span class="selection__result-text">Последний результат</span>`;
+            <span class="selection__result-text">Последний результат</span>`
 
-            li.append(p);
-        }
-
-        list.append(li);
-        buttons.push(button);
-
+      li.append(p)
     }
 
-    return buttons;
+    list.append(li)
+    buttons.push(button)
+  }
 
-};
+  return buttons
+}
 
 //Метод генерации случайной перестановки Фишера-Йетса
 //проходит по массиву в обратном порядке и меняет элемент со случайным элементом перед ним
 
-const shuffle = array => {
+const shuffle = (array) => {
+  const newArray = [...array]
 
-    const newArray = [...array];
+  for (let i = newArray.length - 1; i > 0; i--) {
+    let j = Math.floor(Math.random() * (i + 1))
 
-    for (let i = newArray.length - 1; i > 0; i--) {
+    //деструктуризация
+    ;[newArray[i], newArray[j]] = [newArray[j], newArray[i]]
+  }
 
-        let j = Math.floor(Math.random() * (i + 1));
-
-        //деструктуризация
-        [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-
-    }
-
-    return newArray;
-
+  return newArray
 }
 
-const loadResult = id => {
-
-    return localStorage.getItem(id);
+const loadResult = (id) => {
+  return localStorage.getItem(id)
 }
 
-const createKeyAnswers = data => {
+const createKeyAnswers = (data) => {
+  const keys = []
 
-    const keys = [];
-
-    for (let i = 0; i < data.answers.length; i++) {
-
-        if (data.type === 'radio') {
-
-            keys.push([data.answers[i], !i]);
-        } else {
-
-            keys.push([data.answers[i], i < data.correct]);
-        }
+  for (let i = 0; i < data.answers.length; i++) {
+    if (data.type === 'radio') {
+      keys.push([data.answers[i], !i])
+    } else {
+      keys.push([data.answers[i], i < data.correct])
     }
+  }
 
-    return shuffle(keys); //перемешиваем ответы
-};
+  return shuffle(keys) //перемешиваем ответы
+}
 
-const createAnswer = data => {
+const createAnswer = (data) => {
+  const type = data.type
+  const answers = createKeyAnswers(data)
 
-    const type = data.type;
-    const answers = createKeyAnswers(data);
+  const labels = answers.map((item, i) => {
+    const label = document.createElement('label')
+    label.className = 'answer'
+    const input = document.createElement('input')
+    input.type = type
+    input.className = `answer__${type}`
+    input.name = 'answer'
 
-    const labels = answers.map((item, i) => {
-        const label = document.createElement('label');
-        label.className = 'answer';
-        const input = document.createElement('input');
-        input.type = type;
-        input.className = `answer__${type}`;
-        input.name = 'answer';
+    input.value = i
 
-        input.value = i;
+    const text = document.createTextNode(item[0])
 
-        const text = document.createTextNode(item[0]);
+    label.append(input, text)
 
-        label.append(input, text);
+    return label
+  })
 
-        return label;
+  const keys = answers.map((answer) => answer[1])
 
-    });
-
-    const keys = answers.map(answer => answer[1]);
-
-    return {
-        labels, keys
-    }
-};
+  return {
+    labels,
+    keys,
+  }
+}
 
 const showResult = (result, quiz) => {
+  const block = document.createElement('div')
+  block.className = 'main__box main_box_result result'
 
-    const block = document.createElement('div');
-    block.className = 'main__box main_box_result result';
+  //высчитываем проценты
 
-    //высчитываем проценты
+  const percent = (result / quiz.list.length) * 100
 
-    const percent = result / quiz.list.length * 100;
+  let ratio = 0
 
-    let ratio = 0;
-
-    for (let i = 0; i < quiz.result.length; i++) {
-
-        if (percent >= quiz.result[i][0]) ratio = i;
-
-    }
-    block.innerHTML = `
+  for (let i = 0; i < quiz.result.length; i++) {
+    if (percent >= quiz.result[i][0]) ratio = i
+  }
+  block.innerHTML = `
         <h2 class="main__subtitle main__subtitle_result">Ваш результат</h2>
 
             <div class="result__box">
-                <p class="result__ratio result__ratio_${ratio + 1}">${result}/${quiz.list.length}</p>
+                <p class="result__ratio result__ratio_${ratio + 1}">${result}/${
+    quiz.list.length
+  }</p>
                 <p class="result__text">${quiz.result[ratio][1]}</p>
-            </div>`;
+            </div>`
 
-    const button = document.createElement('button');
-    button.className = 'main__btn result__return';
-    button.textContent = 'К списку квизов';
+  const button = document.createElement('button')
+  button.className = 'main__btn result__return'
+  button.textContent = 'К списку квизов'
 
-    block.append(button);
-    main.append(block);
-    showElem(block);
+  block.append(button)
+  main.append(block)
+  showElem(block)
 
-    button.addEventListener('click', () => {
-        hideElem(block, initQuiz);
-
-    });
-
-};
-
-const saveResult = (result, id) => {
-
-    localStorage.setItem(id, result);
-
+  button.addEventListener('click', () => {
+    hideElem(block, initQuiz)
+  })
 }
 
-const renderQuiz = quiz => {
-    const questionBox = document.createElement('div');
-    questionBox.className = 'main__box main__box-question';
+const saveResult = (result, id) => {
+  localStorage.setItem(id, result)
+}
 
-    hideElem(title);
-    hideElem(selection, () => {
-        showElem(questionBox);
-        main.append(questionBox);
-    });
+const renderQuiz = (quiz) => {
+  const questionBox = document.createElement('div')
+  questionBox.className = 'main__box main__box-question'
 
+  hideElem(title)
+  hideElem(selection, () => {
+    showElem(questionBox)
+    main.append(questionBox)
+  })
 
+  let questionCount = 0
+  let result = 0
 
-    let questionCount = 0;
-    let result = 0;
+  const showQuestion = () => {
+    const data = quiz.list[questionCount]
+    questionCount++
 
-    const showQuestion = () => {
-        const data = quiz.list[questionCount];
-        questionCount++;
+    questionBox.textContent = ''
 
-        questionBox.textContent = '';
+    const form = document.createElement('form')
+    form.className = 'main__form-question'
+    form.dataset.count = `${questionCount}/${quiz.list.length}`
 
-        const form = document.createElement('form');
-        form.className = 'main__form-question';
-        form.dataset.count = `${questionCount}/${quiz.list.length}`;
+    const fieldset = document.createElement('fieldset')
+    const legend = document.createElement('legend')
+    legend.className = 'main__subtitle'
+    legend.textContent = data.question
 
+    const answersData = createAnswer(data)
 
-        const fieldset = document.createElement('fieldset');
-        const legend = document.createElement('legend');
-        legend.className = 'main__subtitle';
-        legend.textContent = data.question;
+    const button = document.createElement('button')
+    button.className = 'main__btn question__next'
+    button.type = 'submit'
+    button.textContent = 'Подтвердить'
 
-        const answersData = createAnswer(data);
+    fieldset.append(legend, ...answersData.labels)
+    form.append(fieldset, button)
 
-        const button = document.createElement('button');
-        button.className = 'main__btn question__next';
-        button.type = 'submit';
-        button.textContent = 'Подтвердить';
+    questionBox.append(form)
 
+    showElem(form)
 
-        fieldset.append(legend, ...answersData.labels);
-        form.append(fieldset, button);
+    form.addEventListener('submit', (event) => {
+      event.preventDefault()
 
-        questionBox.append(form);
+      let ok = false
 
-        showElem(form);
+      const answer = [...form.answer].map((input) => {
+        if (input.checked) ok = true
+        return input.checked ? input.value : false
+      })
 
-        form.addEventListener('submit', event => {
+      if (ok) {
+        if (answer.every((result, i) => !!result === answersData.keys[i])) {
+          result++
+        }
 
-            event.preventDefault();
+        if (questionCount < quiz.list.length) {
+          showQuestion()
+        } else {
+          saveResult(result, quiz.id)
+          hideElem(questionBox, () => {
+            showResult(result, quiz)
+          })
+        }
+      } else {
+        form.classList.add('main__form-question_error')
 
-            let ok = false;
-
-            const answer = [...form.answer].map(input => {
-
-                if (input.checked) ok = true;
-                return input.checked ? input.value : false;
-
-            });
-
-            if (ok) {
-
-                if (answer.every((result, i) => !!result === answersData.keys[i])) {
-
-                    result++;
-
-                }
-
-                if (questionCount < quiz.list.length) {
-
-                    showQuestion();
-
-                } else {
-
-                    saveResult(result, quiz.id);
-                    hideElem(questionBox, () => {
-                        showResult(result, quiz);
-                    });
-
-                }
-
-            } else {
-
-                form.classList.add('main__form-question_error');
-
-                setTimeout(() => {
-                    form.classList.remove('main__form-question_error');
-                }, 1000)
-            }
-        });
-    };
-    showQuestion();
-};
+        setTimeout(() => {
+          form.classList.remove('main__form-question_error')
+        }, 1000)
+      }
+    })
+  }
+  showQuestion()
+}
 
 const addClick = (buttons, data) => {
-    buttons.forEach(btn => {
-        btn.addEventListener('click', () => {
-            const quiz = data.find(item => item.id === btn.dataset.id);
-            renderQuiz(quiz);
-        })
-    });
-};
+  buttons.forEach((btn) => {
+    btn.addEventListener('click', () => {
+      const quiz = data.find((item) => item.id === btn.dataset.id)
+      renderQuiz(quiz)
+    })
+  })
+}
 
 const initQuiz = async () => {
+  showElem(title)
+  showElem(selection)
 
-    showElem(title);
-    showElem(selection);
+  const data = await getData() //data getting from database
 
-    const data = await getData(); //data getting from database
+  const buttons = renderTheme(data) // data rendering
 
-    const buttons = renderTheme(data); // data rendering
+  addClick(buttons, data)
+}
 
-    addClick(buttons, data);
-};
-
-initQuiz();
+initQuiz()
